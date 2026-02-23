75f0e48638e051a01509e69fb1b70cbdd6be1c56
elhageali <131886256+elhageali@users.noreply.github.com>
2026-02-23 13:00:43 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,4 +9,4 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
+} 
