468ef72bfb90f8fa39444a207b857c11281093bd
elhageali <131886256+elhageali@users.noreply.github.com>
2026-02-23 16:53:40 +0100

    Update Terraform infrastructure configuration

--- infra/main.tf
+++ infra/main.tf
@@ -4,9 +4,61 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "pokedeliverydevmmd2026"  
+  name                     = "pokedeliverydevmmd2026"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-} 
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "pokedelivery-dev-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "ai" {
+  name                = "pokedelivery-dev-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.law.id
+}
+
+
+resource "azurerm_service_plan" "plan" {
+  name                = "pokedelivery-dev-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  os_type  = "Linux"
+  sku_name = "Y1" # Consumption
+}
+
+
+resource "azurerm_linux_function_app" "func" {
+  name                = "pokedelivery-dev-func"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "20"
+    }
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "node"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+
+    # App Insights hookup (this also supports your future task 8.3)
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.ai.connection_string
+  }
+}
+
